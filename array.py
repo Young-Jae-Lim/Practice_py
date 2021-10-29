cd3849b8550bd7e52ab879fc6fb577cabf7ad5d2
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2021-10-30 04:20:38 +0900

    Update array.py

--- array.py
+++ array.py
@@ -31,47 +31,48 @@
 A = int(input())
 B = int(input())
 C = int(input())
-
 save = A * B * C
-zero = one = two = three = four = five = six = seven = eight = nine = 0
 save_arr = []
-save_count = []
 for i in str(save):
     save_arr.append(i)
-save_arr = list(map(int, save_arr))
-for j in save_arr:
-    if j == 0:
-        zero +=1
-    elif j == 1:
-        one +=1
-    elif j == 2:
-        two +=1
-    elif j == 3:
-        three +=1
-    elif j == 4:
-        four +=1
-    elif j == 5:
-        five +=1
-    elif j == 6:
-        six +=1
-    elif j == 7:
-        seven +=1 
-    elif j == 8:
-        eight +=1
-    elif j == 9:
-        nine +=1
-save_count.append(zero)
-save_count.append(one)
-save_count.append(two)
-save_count.append(three)
-save_count.append(four)
-save_count.append(five)
-save_count.append(six)
-save_count.append(seven)
-save_count.append(eight)
-save_count.append(nine)
-for k in save_count:
-    print(k)
+for z in range(10):
+    print(save_arr.count(str(z)))
+# save_count = []
+# zero = one = two = three = four = five = six = seven = eight = nine = 0
+# save_arr = list(map(int, save_arr))
+# for j in save_arr:
+#     if j == 0:
+#         zero +=1
+#     elif j == 1:
+#         one +=1
+#     elif j == 2:
+#         two +=1
+#     elif j == 3:
+#         three +=1
+#     elif j == 4:
+#         four +=1
+#     elif j == 5:
+#         five +=1
+#     elif j == 6:
+#         six +=1
+#     elif j == 7:
+#         seven +=1 
+#     elif j == 8:
+#         eight +=1
+#     elif j == 9:
+#         nine +=1
+# save_count.append(zero)
+# save_count.append(one)
+# save_count.append(two)
+# save_count.append(three)
+# save_count.append(four)
+# save_count.append(five)
+# save_count.append(six)
+# save_count.append(seven)
+# save_count.append(eight)
+# save_count.append(nine)
+# for k in save_count:
+#     print(k)
 
-#너무 노가다 한거같음, 수정해야할듯..
-       
+#너무 노가다 한거같음, 수정해야할듯.. 정수형 노가다(swith case문 생각해서 함)
+#중간부분 수정함 한결 간결해짐.       
