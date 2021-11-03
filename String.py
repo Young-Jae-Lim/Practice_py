cb01fd44c15339693230271375ab50255b6c514c
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2021-11-03 18:22:03 +0900

    Create String.py

---
+++ String.py
@@ -0,0 +1,18 @@
+# def change(A):
+#     if str(type(A)) == "<class 'int'>":
+#         print(chr(A))
+#     elif str(type(A)) == "<class 'str'>":
+#         print(ord(A))
+
+# if __name__ == "__main__":
+#     A = input()
+#     change(A)
+
+import sys
+A = int(input())
+B = list(map(str, sys.stdin.readline()))
+if A != (len(B)-1):
+    None
+else:
+    B = [int (i) for i in B[:A]]
+    print(sum(B))
