e77d45117f2897d2de774d8688220b26a824bacc
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2021-11-04 02:37:23 +0900

    Update String.py

--- String.py
+++ String.py
@@ -8,11 +8,24 @@
 #     A = input()
 #     change(A)
 
+# import sys
+# A = int(input())
+# B = list(map(str, sys.stdin.readline()))
+# if A != (len(B)-1):
+#     None
+# else:
+#     B = [int(i) for i in B[:A]]
+#     print(sum(B))
+
 import sys
-A = int(input())
 B = list(map(str, sys.stdin.readline()))
-if A != (len(B)-1):
-    None
-else:
-    B = [int(i) for i in B[:A]]
-    print(sum(B))
+count = []
+for i in range(26):
+    count.append(-1)
+for j in range(len(B)-1):
+    check = ord(B[j])-97
+    if (check) >= 0:
+        if count[check] == -1:
+            count[check] = j
+for z in range(26):
+    print(count[z],end=' ')
