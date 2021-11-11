dacf69790c123fc135d2964a4670e8f4e854b700
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2021-11-12 00:39:22 +0900

    Create math_1.py

---
+++ math_1.py
@@ -0,0 +1,36 @@
+# import sys
+# A, B, C = map(int, sys.stdin.readline().split())
+# if C>B: print(A//(C-B)+1)
+# else: print(-1)
+
+# #런타임 에러 B==C 0일 때 발생하므로 C를 기준으로 잡아야함
+# #A고정, B,C가변이므로 가변끼리 계산하여 고정을 빼면 갯수가 나옴 갯수이므로 +1
+
+# A = int(input())
+# B,C = 2,7
+# count = 1
+# while True:
+#     if A == 1:
+#         print(1)
+#         break
+#     elif B <= A <= C:
+#         print(count+1)
+#         break
+#     else:
+#         B += 6*(count)
+#         C += 6*(count+1)
+#         count+=1 
+
+A = int(input())
+line = 1
+while A > line:
+    A -= line
+    line += 1
+if line % 2 == 0:
+    up = A
+    down = line - A + 1
+else:
+    up = line - A + 1
+    down = A  
+print("{0}/{1}".format(up,down))
+     
