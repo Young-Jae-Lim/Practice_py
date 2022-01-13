f9fe47fac9f2a5988f0ac0031da15a00c8baa3d1
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2022-01-14 01:42:51 +0900

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -18,24 +18,37 @@
 #     print(feeboo(A))
 
 
-def star(n, list):
-    all_star = []
-    if n == 3:
-        return list
-    else:
-        for i in list:
-            all_star.append(i * 3)
-        for i in list:
-            all_star.append(i + " " *len(list) + i)
-        for i in list:
-            all_star.append(i*3)
-        return star(n//3,all_star)
+# def star(n, list):
+#     all_star = []
+#     if n == 3:
+#         return list
+#     else:
+#         for i in list:
+#             all_star.append(i * 3)
+#         for i in list:
+#             all_star.append(i + " " *len(list) + i)
+#         for i in list:
+#             all_star.append(i*3)
+#         return star(n//3,all_star)
     
+# if __name__ == "__main__":
+#     A = int(input())
+#     first_start = ["***", "* *", "***"]
+#     all = star(A,first_start)
+#     for i in all:
+#         print(i)
+        
+# #참고 : https://imgzon.tistory.com/37
+
+def hanoi(n,start,tmp,end):
+    if n == 1:
+        print(start,end)
+    else:
+        hanoi(n-1,start,end,tmp)
+        print(start,end)
+        hanoi(n-1,tmp,start,end)
+
 if __name__ == "__main__":
     A = int(input())
-    first_start = ["***", "* *", "***"]
-    all = star(A,first_start)
-    for i in all:
-        print(i)
-        
-#참고 : https://imgzon.tistory.com/37
+    print((2**A)-1)
+    hanoi(A,1,2,3)
