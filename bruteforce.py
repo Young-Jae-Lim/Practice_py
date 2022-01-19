72f6ab6314ba323f51628dda17eb0246399066ef
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2022-01-19 22:38:46 +0900

    Update sort.py

--- bruteforce.py
+++ bruteforce.py
@@ -30,22 +30,59 @@
 
 # #하다가 꼬인듯.. 참고: https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-2231%EB%B2%88-%EB%B6%84%ED%95%B4%ED%95%A9-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
 
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# ans_save = []
+# for i in range(A):
+#     B,C = map(int,sys.stdin.readline().split())
+#     save.append([B,C])
+    
+# for i in save:
+#     count = 0
+#     for j in save:
+#         if j[0] > i[0] and j[1] > i[1]:
+#             count+=1
+#     ans_save.append(count+1)
+    
+# print(' '.join(str(i) for i in ans_save))
+
+# import sys
+# A,B = map(int, sys.stdin.readline().split())
+# save = []
+# solve_save = []
+# for i in range(A):
+#     save.append(input())
+       
+# for i in range(A-7):
+#     for j in range(B-7):
+#         count1 = 0
+#         count2 = 0
+#         for z in range(i, i+8):
+#             for k in range(j, j+8):
+#                 if (z+k) % 2 == 0:
+#                     if save[z][k] !="W":
+#                         count1+=1
+#                     else:
+#                         count2+=1
+#                 else:
+#                     if save[z][k] !="B":
+#                         count1+=1
+#                     else:
+#                         count2+=1
+#         solve_save.append(count1)
+#         solve_save.append(count2)
+# print(min(solve_save))
+# 참고: https://jennnn.tistory.com/6       
+
 import sys
 A = int(sys.stdin.readline())
-save = []
-ans_save = []
-for i in range(A):
-    B,C = map(int,sys.stdin.readline().split())
-    save.append([B,C])
-    
-for i in save:
-    count = 0
-    for j in save:
-        if i == j:
-            pass
-        else:
-            if j[0] > i[0] and j[1] > i[1]:
-                count+=1
-    ans_save.append(count+1)
-    
-print(' '.join(str(i) for i in ans_save))
+end = 666
+count = 0
+while True:
+    if "666" in str(end):
+        count +=1
+    if count == A:
+        print(end)
+        break
+    end+=1     
---
+++ sort.py
@@ -0,0 +1,101 @@
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     B = int(sys.stdin.readline())
+#     save.append(B)
+# for i in range(len(save)):
+#     min1 = min(save)
+#     print(min1)
+#     save.remove(min1)
+    
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     B = int(sys.stdin.readline())
+#     save.append(B)
+# save.sort()
+# for i in save:
+#     print(i)
+
+# import sys
+# A = int(sys.stdin.readline())
+# check = [0] * 10001
+# for i in range(A):
+#     check[int(sys.stdin.readline())]+=1
+# for i in range(10001):
+#     if check[i]!=0:
+#         for j in range(check[i]):
+#             print(i)
+# #참고: https://pacific-ocean.tistory.com/67
+
+# import sys
+# from collections import Counter
+# A= int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     save.append(int(sys.stdin.readline()))
+# save.sort()
+# print(round(sum(save)/A))
+# print(save[A//2])
+# save_count = Counter(save).most_common()
+# if len(save_count) > 1:
+#     if save_count[0][1] == save_count[1][1]:
+#         print(save_count[1][0])
+#     else:
+#         print(save_count[0][0])
+# else:
+#     print(save_count[0][0])        
+# print(max(save)-min(save))
+#참고: https://velog.io/@jaenny/%EB%B0%B1%EC%A4%80-2108-%ED%86%B5%EA%B3%84%ED%95%99-Python%ED%8C%8C%EC%9D%B4%EC%8D%AC
+#다시볼 필요가 있음 round, Count함수 알아둘것.
+
+# import sys
+# A = list(sys.stdin.readline())
+# A.sort(reverse=True)
+# A.remove('\n')
+# print("".join(A))
+
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     B = list(map(int, sys.stdin.readline().split()))
+#     save.append(B)
+# save.sort()
+# for i in save:
+#     print(str(i[0])+' '+str(i[1]))
+
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     B = list(map(int, sys.stdin.readline().split()))
+#     save.append(B)
+# save.sort(key = lambda x:(x[1],x[0]))
+# for i in save:
+#     print(str(i[0])+' '+str(i[1]))
+#lambda 참고: https://dev-note-97.tistory.com/13
+
+# import sys
+# A = int(sys.stdin.readline())
+# save = []
+# for i in range(A):
+#     B = str(sys.stdin.readline())
+#     save.append(B)
+# save = list(set(save))
+# save.sort(key = lambda x:(len(x), x))
+# for i in save:
+#     i = str(i).strip()
+#     print(i)
+
+import sys
+A = int(sys.stdin.readline())
+save = []
+for i in range(A):
+    B = list(map(str, sys.stdin.readline().split()))
+    save.append(B)
+save.sort(key = lambda x:int(x[0]))
+for i in save:
+    print(i[0],i[1])
